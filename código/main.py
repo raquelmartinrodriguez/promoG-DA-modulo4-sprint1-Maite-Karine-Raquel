d6288b70d0a70f1146d523f66aa90499215d3b5f
karinem0 <tu_correo_electronico@example.com>
2024-04-11 10:26:46 +0200

    archivos exploracion

--- código/main.py
+++ código/main.py
@@ -4,5 +4,29 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 #%%
-df_Quality_continent = exploracion.cargar_dataframes("promoG-DA-modulo4-sprint1-Maite-Karine-Raquel/data/Coffee_Qlty_By_Continent.csv", True)
+
+ruta_continente = "../data/Coffee_Qlty_By_Continent.csv"
+ruta_país = "../data/Coffee_Qlty_By_Country.csv"
+ruta_calidad = "../data/Coffee_Qlty.csv"
+ruta_consumo = "../data/CoffeeConsumption.csv"
+
+df_quality_continent, df_quality_country = exploracion.cargar_dataframes(ruta_continente, ruta_país)
+df_quality, df_consumption = exploracion.cargar_dataframes(ruta_calidad, ruta_consumo)
+
 # %%
+df_quality_continent.head(5)
+# %%
+df_quality_country.head(5)
+# %%
+df_quality.head(5)
+# %%
+df_consumption.head(5)
+# %%
+# Configurar la visualización
+exploracion.configurar_visualizacion()
+
+# %%
+dataframes = [df_consumption, df_quality, df_quality_continent, df_quality_country]
+
+for df in dataframes: 
+    exploracion.exploracion_dataframe(df)
